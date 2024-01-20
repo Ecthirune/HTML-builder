867c6edccadb67b2e4e02afd066b8559ff8f7a28
Ecthirune <overmusko@gmail.com>
2024-01-20 14:57:49 +0300

    feat: done copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'files');
+const destinationDir = path.join(__dirname, 'files-copy');
+
+function copyDir(source, destination) {
+  fs.mkdir(destination, { recursive: true }, (err) => {
+    if (err) {
+      console.log('Error creating destination directory:', err);
+      return;
+    }
+
+    fs.readdir(source, (err, files) => {
+      if (err) {
+        console.log('Error reading source directory:', err);
+        return;
+      }
+
+      files.forEach((file) => {
+        const sourcePath = path.join(source, file);
+        const destinationPath = path.join(destination, file);
+
+        fs.stat(sourcePath, (err, stats) => {
+          if (err) {
+            console.log(`Error retrieving file stats for ${file}:`, err);
+            return;
+          }
+
+          if (stats.isFile()) {
+            fs.copyFile(sourcePath, destinationPath, (err) => {
+              if (err) {
+                console.log(`Error copying file ${file}:`, err);
+              }
+            });
+          } else if (stats.isDirectory()) {
+            copyDir(sourcePath, destinationPath);
+          }
+        });
+      });
+    });
+  });
+}
+
+copyDir(sourceDir, destinationDir);

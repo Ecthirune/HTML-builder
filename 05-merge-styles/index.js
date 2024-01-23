6df6c17ef56ca113b8f73f59d026e67242decc56
Ecthirune <overmusko@gmail.com>
2024-01-23 12:13:34 +0300

    fix: add file/folder creations to 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,8 +2,52 @@ const fs = require('fs');
 const path = require('path');
 
 const stylesDir = path.join(__dirname, 'styles');
+const targetFolderName = 'project-dist';
+
+
+createFolder(__dirname, targetFolderName);
 const distDir = path.join(__dirname, 'project-dist');
+createFile(distDir, 'bundle.css');
 const bundleFilePath = path.join(distDir, 'bundle.css');
+
+async function createFolder(__dirname, name) {
+  const toCreate = path.join(__dirname, name);
+  await fs.access(toCreate, (err) => {
+    if (err) {
+      fs.mkdir(toCreate, { recursive: true }, (err) => {
+        if (err) {
+          console.log('Error creating destination directory:', err);
+          return;
+        } else {
+          console.log('Directory created: ', toCreate);
+        }
+      });
+    } else {
+      console.log('Directory already exists:', toCreate);
+      return;
+    }
+  });
+}
+
+async function createFile(directory, name) {
+  const filePath = path.join(directory, name);
+  await fs.access(filePath, (err) => {
+    if (err) {
+      fs.writeFile(filePath, '', (err) => {
+        if (err) {
+          console.log('Error:', err, ' creating file in: ', filePath);
+          return;
+        } else {
+          console.log('File created: ', filePath);
+        }
+      });
+    } else {
+      console.log('File already exists: ', filePath);
+      return;
+    }
+  });
+}
+
 
 function buildCSSBundle() {
   fs.readdir(stylesDir, (err, files) => {
@@ -15,7 +59,7 @@ function buildCSSBundle() {
     const cssFiles = files.filter((file) => path.extname(file) === '.css');
 
     const styles = [];
-
+    
     cssFiles.forEach((file) => {
       const filePath = path.join(stylesDir, file);
 
